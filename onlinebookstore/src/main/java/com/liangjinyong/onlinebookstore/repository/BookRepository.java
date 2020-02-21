295d6cdb02e60e805b311a7dc912b4865a12de1a
ljy <393941932@qq.com>
2020-02-21 17:34:42 +0900

    Create angular project and completed the feature 1.

--- onlinebookstore/src/main/java/com/liangjinyong/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/liangjinyong/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.liangjinyong.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.liangjinyong.onlinebookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
